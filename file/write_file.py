7409e73ce1ff5cf641893934b5dc21cb41de1995
RavalAnkesh <151333058+RavalAnkesh@users.noreply.github.com>
2026-01-05 08:11:25 +0530

    Update write_file.py
    Add comments

--- file/write_file.py
+++ file/write_file.py
@@ -1,4 +1,9 @@
+#open file
 f=open('student.txt','w')
+
+#write in file
 f.write("this is example of file creation\n")
 f.write("we can here create new file,write in file,append in file etc")
+
+#close file
 f.close()
