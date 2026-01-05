7a5f2ecbceec863517ee38ba5131b1fab4c806e5
RavalAnkesh <151333058+RavalAnkesh@users.noreply.github.com>
2026-01-05 08:13:39 +0530

    Update arithmetic.py
    Add Comments

--- mypackage/arithmetic.py
+++ mypackage/arithmetic.py
@@ -1,11 +1,14 @@
+# Arithmetic Operators
+
+#addition
 def add(a,b):
     return a + b
-
+#Substraction
 def sub(a,b):
     return a - b
-
+#Multiplication
 def mul(a,b):
     return a * b
-
+#division
 def div(a,b):
     return a / b
